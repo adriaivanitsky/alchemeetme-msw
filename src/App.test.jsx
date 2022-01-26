15cfff6b1317723f654350a7c7dd4841b9f2be97
Adria Ivanitsky <littleivanitsky@gmail.com>
2022-01-26 15:02:45 -0800

    got first test passing

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,16 +2,15 @@
 // 🚨🚨 https://mswjs.io/docs/ 🚨🚨
 
 import { screen, render } from '@testing-library/react'
-// 🚨
-// import rest
-// import setupServer
+import { rest } from 'msw'
+import { setupServer } from 'msw/node'
+
 import App from './App'
 
 const user = {
   id: 1,
   created_at: '2021-12-13T00:17:29+00:00',
-  // 🚨 Add a name here
-  name: '',
+  name: 'Vonta',
   avatar: 'https://thumbs.gfycat.com/NiceRequiredGrunion-size_restricted.gif',
   header: 'https://static.wikia.nocookie.net/naruto/images/5/50/Team_Kakashi.png',
   likes: ['React', 'Anime', 'Traveling', 'Living', 'Tower Defense Games', 'Card Games'],
@@ -20,12 +19,17 @@ const user = {
 }
 
 // 🚨 Create your server
+const server = setupServer(
+  rest.get('https://uzgiamkrbapxufnwdrja.supabase.co', (req, res, ctx) => {
+    return res(ctx.json(user))
+  })
+)
 
 // 🚨 Listen for server start
-beforeAll()
+beforeAll(() => server.listen())
 
 // 🚨 Close server when complete
-afterAll()
+afterAll(() => server.close())
 
 test('Should render the header', async () => {
   render(<App />)
